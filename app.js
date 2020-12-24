1bef74b220e022a914e34006a9abaa4d3c97df6f
Sana Khalid <sanabibi.khalid@gmail.com>
2020-12-23 19:58:33 -0800

    final edits, cleaning up code

--- app.js
+++ app.js
@@ -12,28 +12,52 @@ function computerPlay() {
 }
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
 
-    playerSelection = prompt("Do you choose: rock, paper, or scissors?");
+    let playerSelection = prompt("Do you choose: rock, paper, or scissors?");
 
     let user = playerSelection.toLowerCase();
 
-    computerSelection = computerPlay();
+    let computerSelection = computerPlay();
 
     if (user === computerSelection) {
         return "It's a tie!";
-    } else if ((user === "rock" && computerSelection === "scissors") ||
-        (user === "paper" && computerSelection === "rock") ||
+    } else if ((user === "rock" && computerSelection === "scissors")
+        ||
+        (user === "paper" && computerSelection === "rock")
+        ||
         (user === "scissors" && computerSelection === "paper")) {
-        return `You win! ${user} beats ${computerSelection}`;
+        // User wins
+        return 1;
     } else {
-        return `You lose! ${computerSelection} beats ${user}`;
+        // Computer wins
+        return 2;
     }
-
 }
 
-// function game(playRound){
-//     let userScore= 0;
-//     let computerScore=0;
 
-// }
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        if (result === 1) {
+            userScore++;
+        } else if (result === 2) {
+            computerScore++;
+        }
+        console.log(i);
+    }
+
+    if (userScore > computerScore) {
+        return "User wins!"
+    } else if (computerScore > userScore) {
+        return "Computer wins!"
+    } else if (userScore === computerScore) {
+        return "It's a tie!"
+    }
+}
+
+console.log(game());
